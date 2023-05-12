6560df33f9e3179c6936069509aae3d083aa3bd4
Daniele Fea <daniele.fea@vallauri.edu>
2023-05-12 13:03:29 +0200

    Inserita navbar e Carosello

---
+++ index.js
@@ -0,0 +1,22 @@
+var carosello = [
+    {
+        imgpath: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DVNitrZxzAXw&psig=AOvVaw2L-b7CuV_v-bmoeR7NGREq&ust=1683975695217000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPDZ6bzQ7_4CFQAAAAAdAAAAABAD",
+        titolo: "Risolvi gli esercizi",
+        desc:"...."
+    },
+    {
+        imgpath: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DVNitrZxzAXw&psig=AOvVaw2L-b7CuV_v-bmoeR7NGREq&ust=1683975695217000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPDZ6bzQ7_4CFQAAAAAdAAAAABAD",
+        titolo: "Risolvi gli esercizi",
+        desc:"...."
+    },
+    {
+        imgpath: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DVNitrZxzAXw&psig=AOvVaw2L-b7CuV_v-bmoeR7NGREq&ust=1683975695217000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPDZ6bzQ7_4CFQAAAAAdAAAAABAD",
+        titolo: "Risolvi gli esercizi",
+        desc:"...."
+    },
+    {
+        imgpath: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DVNitrZxzAXw&psig=AOvVaw2L-b7CuV_v-bmoeR7NGREq&ust=1683975695217000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPDZ6bzQ7_4CFQAAAAAdAAAAABAD",
+        titolo: "Risolvi gli esercizi",
+        desc:"...."
+    }
+];
