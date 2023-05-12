26e276331c43dc574316edf34a67bd414055f752
Daniele Fea <daniele.fea@vallauri.edu>
2023-05-12 13:20:21 +0200

    Correzione link immagini

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var carosello = [
     {
-        imgpath: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DVNitrZxzAXw&psig=AOvVaw2L-b7CuV_v-bmoeR7NGREq&ust=1683975695217000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPDZ6bzQ7_4CFQAAAAAdAAAAABAD",
+        imgpath: "https://www.tecnicadellascuola.it/wp-content/uploads/2022/07/Matematica_Casella-300x194.jpg",
         titolo: "Risolvi gli esercizi",
         desc:"...."
     },
     {
-        imgpath: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DVNitrZxzAXw&psig=AOvVaw2L-b7CuV_v-bmoeR7NGREq&ust=1683975695217000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPDZ6bzQ7_4CFQAAAAAdAAAAABAD",
+        imgpath: "https://www.tecnicadellascuola.it/wp-content/uploads/2022/07/Matematica_Casella-300x194.jpg",
         titolo: "Risolvi gli esercizi",
         desc:"...."
     },
     {
-        imgpath: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DVNitrZxzAXw&psig=AOvVaw2L-b7CuV_v-bmoeR7NGREq&ust=1683975695217000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPDZ6bzQ7_4CFQAAAAAdAAAAABAD",
+        imgpath: "https://www.tecnicadellascuola.it/wp-content/uploads/2022/07/Matematica_Casella-300x194.jpg",
         titolo: "Risolvi gli esercizi",
         desc:"...."
     },
     {
-        imgpath: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DVNitrZxzAXw&psig=AOvVaw2L-b7CuV_v-bmoeR7NGREq&ust=1683975695217000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPDZ6bzQ7_4CFQAAAAAdAAAAABAD",
+        imgpath: "https://www.tecnicadellascuola.it/wp-content/uploads/2022/07/Matematica_Casella-300x194.jpg",
         titolo: "Risolvi gli esercizi",
         desc:"...."
     }
