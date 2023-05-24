617d7e3902190f1d15dc90ebb2ee7a087c37c727
Marco Pecoraro <122470821+Marco-Pecoraro@users.noreply.github.com>
2023-05-24 14:30:31 +0000

    Aggiunte scelte Studente / Docente

--- iscriviti.js
+++ iscriviti.js
@@ -1,4 +1,13 @@
 document.getElementsByClassName("btn-continua")[0].addEventListener("click", function () {
     /* Aggiungere controllo che l'input username non sia string.empty */
-    document.getElementById("input-password").classList.remove("d-none")
+    document.getElementById("input-choice").classList.remove("d-none") // Si rimuove la classe Display None, in modo che apparga il prossimo campo da compilare
+})
+
+document.getElementById("scelta-studente").addEventListener("click", function () { // Funzione che gestisce la scelta 'Studente'
+    document.getElementById("level-choice").classList.remove("d-none")
+    document.getElementsByClassName("btn-continua")[0].textContent = "Registrati"
+})
+
+document.getElementById("scelta-docente").addEventListener("click", function () { // Funzione che gestisce la scelta 'Docente'
+    document.getElementsByClassName("btn-continua")[0].textContent = "Registrati"
 })
