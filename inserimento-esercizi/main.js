56dbe8edf73a1295d5f7fde5489d671d850b52d7
jiayi zhan <jiayizhan2005@gmail.com>
2023-05-29 23:34:27 +0200

    Aggiornato il design

--- inserimento-esercizi/main.js
+++ inserimento-esercizi/main.js
@@ -1,9 +1,27 @@
+//#region
+// dati dall'esterno
 
-const isInsegnante = false;
+const STATUS = {
+  LOGGED_OUT: "LOGGED_OUT",
+  LOGGED_IN_STUDENTE: "LOGGED_IN_STUDENTE",
+  LOGGED_IN_INSEGNANTE: "LOGGED_IN_INSEGNANTE",
+};
 
-const argomenti = [
+const statoAttuale = STATUS.LOGGED_IN_INSEGNANTE;
+
+const ARGOMENTO = [
   {
     id: 1,
     argomento: "Prova1",
+    difficoltà: 5,
+    classe: 4,
   },
 ];
+
+//#endregion
+
+const formEsercizio = document.getElementById("form-crea-esercizio");
+
+formEsercizio.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
