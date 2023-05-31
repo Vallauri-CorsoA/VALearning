7e5f6e9a62748643dcf9f6a16c23aa3a4283f614
amine <120721423+yungestAmine@users.noreply.github.com>
2023-05-31 15:26:43 +0000

    completato oggettiBase.js

--- JS/oggettiBase.js
+++ JS/oggettiBase.js
@@ -4,6 +4,7 @@
 //  0 -> Scelta Multipla                     //
 //  1 -> Completa gli spazi                  //
 //  2 -> Vero o Falso                        //
+//                                           //
 //###########################################//
 
 
@@ -40,13 +41,23 @@ let esercizio = {
         {
             domanda: "Who is a murder?",
             opzioni: ["A person who steals in markets", "A person who cheat at school", "A person who killed someone", "None of the previous"],
-            rispostaData: "A person who killed someone",
             rispostaCorretta: "A person who killed someone",
             punteggio: 0
         },
         tipo1:{
             domanda: "Fill the gaps",
-            
+            risposteCorrette:[
+                "ever",
+                "their",
+                "don't",
+                "phone"
+            ],
+            punteggio: [0,0,0,0]
+        },
+        tipo2:{
+            domanda:"Is King Von died?",
+            rispostaCorretta: "F",
+            punteggio: 0
         }
     }
 };
@@ -54,10 +65,18 @@ let correzione = {
     idUtente:2704,
     idEsercizio:0,
     punteggioFinale:0,
-    risposteDate: {
-        es0: "A person who killed someone",
-        es1:[
-            ""
-        ]
+    risposte: {
+        tipo0: "A person who killed someone",
+        tipo1:[
+            "ever",
+            "them",
+            "don't",
+            "phone"
+        ],
+        tipo2:"V"
     }
 };
+
+/*
+    If you ever asked someone for their mobile phone number and they said, “Sorry, I don’t own a mobile phone,”
+*/
