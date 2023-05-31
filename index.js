0f5a76e2a9257ab3700702ac0c3d720e1c77d27d
Giacomo Isoardi <89302788+Jackiso2006@users.noreply.github.com>
2023-05-31 10:51:20 +0200

    aggiunta tipi risposte

--- index.js
+++ index.js
@@ -1,22 +1,65 @@
-var carosello = [
-    {
-        imgpath: "https://www.tecnicadellascuola.it/wp-content/uploads/2022/07/Matematica_Casella-300x194.jpg",
-        titolo: "Risolvi gli esercizi",
-        desc:"...."
-    },
-    {
-        imgpath: "https://www.tecnicadellascuola.it/wp-content/uploads/2022/07/Matematica_Casella-300x194.jpg",
-        titolo: "Risolvi gli esercizi",
-        desc:"...."
-    },
-    {
-        imgpath: "https://www.tecnicadellascuola.it/wp-content/uploads/2022/07/Matematica_Casella-300x194.jpg",
-        titolo: "Risolvi gli esercizi",
-        desc:"...."
-    },
-    {
-        imgpath: "https://www.tecnicadellascuola.it/wp-content/uploads/2022/07/Matematica_Casella-300x194.jpg",
-        titolo: "Risolvi gli esercizi",
-        desc:"...."
+window.addEventListener("load", function(){
+    document.getElementsByClassName("openES")[0].addEventListener("click", function(){
+        creaEsVF(esercizioVF); // non mi sono venuti in mente altri modi per passare paramentri con l'addeventlistener
+    });
+    document.getElementsByClassName("openES")[1].addEventListener("click", function(){
+        creaEsTB(esercizioTB); 
+    });
+    document.getElementsByClassName("openES")[2].addEventListener("click", function(){
+        creaEsRM(esercizioRM);
+    });
+});
+
+
+function creaEsVF(esercizio){
+    let aus = "<div class='tags'>";
+    for(let i of esercizio.argomento){
+        aus += `<span class="badge badge-info">${i}   </span>`;
     }
-];
+    aus += `</div>
+            <p>${esercizio.consegna}</p>
+            <div>
+                <input type="radio" id="v">
+                <label for="v">V</label>
+                <input type="radio" id="f">
+                <label for="f">F</label>
+            </div>
+            <button onclick="correggi(${esercizio})">CORREGGI</button>`;
+
+    document.getElementsByClassName("esercizioVF")[0].innerHTML = aus;
+}
+
+function creaEsTB(esercizio){
+    let aus = "<div class='tags'>";
+    for(let i of esercizio.argomento){
+        aus += `<span>${i}   </span>`;
+    }
+    aus += `</div>
+            <p>${esercizio.consegna}</p>
+            <p>${esercizio.testoPrima}</p>
+            <input type="text">
+            <p>${esercizio.testoDopo}</p>
+            <button onclick="correggi(${esercizio})">CORREGGI</button>`;
+
+    document.getElementsByClassName("esercizioTB")[0].innerHTML = aus;
+}
+
+function creaEsRM(esercizio){
+    let aus = "<div class='tags'>";
+    for(let i of esercizio.argomento){
+        aus += `<span>${i}   </span>`;
+    }
+    aus += `</div>
+            <p>${esercizio.consegna}</p>
+            <div>`;
+
+    for(let i in esercizio.scelte){
+        aus+=`
+        <input type="radio" id="ris${i}">
+        <label for="ris${i}">${esercizio.scelte[i]}</label>`;
+    }
+    aus += `</div>
+    <button onclick="correggi(${esercizio})">CORREGGI</button>`;
+
+    document.getElementsByClassName("esercizioRM")[0].innerHTML = aus;
+}
--- oggettiBase.js
+++ oggettiBase.js
@@ -1,78 +1,52 @@
-/*
-    Oggetto completo relativo ad UN esercizio/domanda
+/* In base al tipo interpreto l'oggetto risposte in maniera opportuna */
+/**
+ * TIPI:
+ * - risposta multipla
+ * - testo bucato
+ * - vero/falso
+ */
 
-*/
-let esercizio = {
-    /* Id che identifica univocamente l'esercizio */
+let esercizioVF = { //VF = vero falso
     id:0,
-    /* 
-        argomento e difficoltà vegono visualizzati in html
-        come fossero tag 
-    */
-    /* es. verbi, reading, ... */
-    argomento:"",
-    /* Numero da ... a ... */
-    difficolta: 0,
-    /* Testo dell'esercizio/domanda */
-    consegna: "",
-    
-    /* In base al tipo interpreto l'oggetto risposte in maniera opportuna */
-    /**
-     * TIPI:
-     * - risposta multipla
-     * - testo bucato
-     * - vero/falso
-     */
-    tipo:"",
+    argomento:["verbi", "reading"], 
+    difficolta: 4,
+    consegna: "questa è la domanda del vero/falso",
     risposte:{
         /* Risposte possibili e corrette */
-        punteggio:0/* Punteggi per ogni risposta! */
+        punteggio:5,/* Punteggi per ogni risposta! */
+        risData: "",
+        risCorretta: "v"
     }
 };
 
-/*
-    Correzione di un esercizio/insieme di esercizi
-*/
-let correzione = {
-    /* id dell'utente che ha svolto l'esercizio */
-    idUtente:0,
-    /**  id dell'esercizio svolto*/ 
-    idEsercizio:0,
-
-    /* Punteggio finale (dopo la correzione) */
-    punteggioFinale:0,
-    
-    /* Risposte utente -> la struttura dipende dal tipo dell'esercizio */
-    risposteDate: {}
+let esercizioRM = { //RM = risposta multipla
+    id: 0,
+    argomento:["verbi", "reading"],
+    difficolta: 4,
+    consegna: "questa è la domanda con possibili risposte multiple",
+    scelte: [
+        "risposta1",
+        "risposta2",
+        "risposta3",
+        "risposta4"
+    ],
+    risposte:{
+        punteggio: 5,
+        risData: "",
+        risCorretta: "risposta4"
+    }
 };
 
-
-let profiloUtente = {
-    /* s-> studente, d->docente */
-    tipo:"s",
-    idUtente = 0,
-    /* Modificabile nella pagina di profilo */
-    classe = 0,
-    nome:"", 
-    cognome:"",
-    email:"",
-    
-    /* Per ogni argomento (o classe di argomento) indicare 
-    il livello di conoscenza/esercizio/abilità */
-    livello:{},
-
-    /*
-        - argomenti degli esercizi svolti (calcolati)
-        - contenuti preferiti (inseriti)
-    */
-    preferenze:{},
-
-    documentiInseriti:[]
+let esercizioTB = { //TB = testo bucato
+    id: 0,
+    argomento:["verbi", "reading"],
+    difficolta: 4,
+    consegna: "questa è la domanda in cui devi inserire la parola giusta nei buchi del testo",
+    testoPrima: "Questo è il testo prima del buco",
+    testoDopo: "Questo è il testo dopo il buco",
+    risposte:{
+        punteggio: 5,
+        risData: "",
+        risCorretta: "buco"
+    }
 };
-
-let documento = {
-    path:"",
-    nome:"",
-    argomenti:"",
-    idProprietario:0
-}
