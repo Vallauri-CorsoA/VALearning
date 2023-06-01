9e9b317407014cd6ef47aedc41ee737261c0f71c
coding like a pro <120721713+DemichelisAlessandro@users.noreply.github.com>
2023-06-01 06:36:07 +0000

    aggiunto alcuni commenti nel file js per migliorare la comprensibilità

--- JS/oggettiBase.js
+++ JS/oggettiBase.js
@@ -8,43 +8,39 @@
 //###########################################//
 
 
-let profiloUtente = {
-    /* s-> studente, d->docente */
-    tipo:"s",
+let profiloUtente = {//dati relativi all'utente
+    tipo:"s",/* s-> studente, d->docente */
     idUtente : 2704, /* Modificabile nella pagina di profilo */
-    classe : "3A Informatica",
-    nome:"King", 
-    cognome:"Von",
-    email:"kingvon911@gmail.com",
-    livello:"B1",
-    preferenze:[ 0, 2 ],
-    documentiInseriti:[]
+    classe : "3A Informatica",//classe dell'utente
+    nome:"King", //nome dell'utente
+    cognome:"Von",//cognome dell'utente
+    email:"kingvon911@gmail.com",//email dell'utente
+    livello:"B1",//livello di conoscenza dell'inglese dell'utente
+    preferenze:[ 0, 2 ],//preferenze dell'utente
+    documentiInseriti:[]//documenti inseriti dall'uente nel forum                 
 };
 
-let documento = {
-    path:"",
-    nome:"",
-    argomenti:"",
-    idProprietario:0
+let documento = {//dati relativi al documento postato sul forum
+    path:"",//percorso del file 
+    nome:"",//nome del file 
+    argomenti:"",//argomento del file
+    idProprietario:0//id dell'utente che ha postato il documento
 }
-let esercizio = {
-    /* Id che identifica univocamente l'esercizio */
-    id:0,
-    argomento:"Crimes",
-    /* Numero da ... a ... */
-    difficolta: 1,
-    /* Testo dell'esercizio/domanda */
-    consegna: "Completa gli esercizi",
-    tipo:0,
-    risposte:{
-        tipo0:
+let esercizio = {//dati relativi all'esercizio svolto
+    id:0,/* Id che identifica univocamente l'esercizio */
+    argomento:"Crimes",//argomento dell'esercizio
+    difficolta: 1,//difficoltà dell'esercizio
+    consegna: "Completa gli esercizi",/* Testo dell'esercizio/domanda */
+    tipo:0,//tipo dell'esercizio 0 || 1 || 2
+    risposte:{//parte reltiva alle tre tipologie di esercizio
+        tipo0://domanda a risposte multiple
         {
-            domanda: "Who is a murder?",
-            opzioni: ["A person who steals in markets", "A person who cheat at school", "A person who killed someone", "None of the previous"],
-            rispostaCorretta: "A person who killed someone",
-            punteggio: 0
+            domanda: "Who is a murder?",//domanda dell'esercizio
+            opzioni: ["A person who steals in markets", "A person who cheat at school", "A person who killed someone", "None of the previous"],//le possibili risposte che l'utente può scegliere
+            rispostaCorretta: "A person who killed someone",//la risposta corretta
+            punteggio: 0//il punteggio dell'esercizio
         },
-        tipo1:{
+        tipo1:{//riempi il testo bucato
             domanda: "Fill the gaps",
             risposteCorrette:[
                 "ever",
